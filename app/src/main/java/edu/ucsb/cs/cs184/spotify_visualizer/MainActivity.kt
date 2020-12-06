e577e83d54100333d036c854b9d4f71f8e91bf4d
dkupshm <dkupsh@ucsb.edu>
2020-12-06 10:49:27 -0800

    Added Spotify Authentification

--- app/src/main/java/edu/ucsb/cs/cs184/spotify_visualizer/MainActivity.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/spotify_visualizer/MainActivity.kt
@@ -1,21 +1,90 @@
 package edu.ucsb.cs.cs184.spotify_visualizer
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.protocol.types.PlayerState
+import com.spotify.protocol.types.Track
+import kotlinx.coroutines.launch
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val CLIENT_ID = "aac5e0cf9ed24f71868e35c1faeeba9c"
+    private val REDIRECT_URI = "http://edu.ucsb.cs.cs184.spotify_visualizer/callback"
+    private var mSpotifyAppRemote: SpotifyAppRemote? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var btn = findViewById<Button>(R.id.spotify_login_btn)
         //set button click to send to Spotify API Login
         btn.setOnClickListener {
-            val int = Intent(this,Main_Screen::class.java)
+            connect(true)
+            val int = Intent(this, Main_Screen::class.java)
             startActivity(int)
         }
+
+    }
+
+    override fun onStart() {
+        Log.d("Main Activity", "On Start!")
+        super.onStart()
+
+    }
+
+    private fun connect(showAuthView: Boolean) {
+
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote)
+        lifecycleScope.launch {
+            try {
+                mSpotifyAppRemote = connectToAppRemote(showAuthView)
+                onConnected()
+            } catch (error: Throwable) {
+                Log.e("Main Activity", error.toString())
+                onDisconnected()
+            }
+        }
+    }
+
+    private fun onConnected() {
+        Log.d("Main Activity", "Connected")
+    }
+
+    private fun onDisconnected() {
+        Log.d("Main Activity", "Disconnected")
+    }
+
+    private suspend fun connectToAppRemote(showAuthView: Boolean): SpotifyAppRemote? =
+            suspendCoroutine { cont: Continuation<SpotifyAppRemote> ->
+                SpotifyAppRemote.connect(
+                        application,
+                        ConnectionParams.Builder(CLIENT_ID)
+                                .setRedirectUri(REDIRECT_URI)
+                                .showAuthView(showAuthView)
+                                .build(),
+                        object : Connector.ConnectionListener {
+                            override fun onConnected(spotifyAppRemote: SpotifyAppRemote) {
+                                cont.resume(spotifyAppRemote)
+                            }
+
+                            override fun onFailure(error: Throwable) {
+                                cont.resumeWithException(error)
+                            }
+                        })
+            }
+
+    override fun onStop() {
+        super.onStop()
     }
 }
