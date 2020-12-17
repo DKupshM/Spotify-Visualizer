fc9b4beccc1cce95da9b7ef299ebfe2b15242cf1
dkupshm <dkupsh@ucsb.edu>
2020-12-17 08:55:29 -0800

    Album Covers

--- app/src/main/java/edu/ucsb/cs/cs184/spotify_visualizer/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/spotify_visualizer/ui/dashboard/DashboardFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -35,15 +36,18 @@ class DashboardFragment : Fragment() {
 
 
         val rvSongs = root.findViewById<View>(R.id.searched_Songs) as RecyclerView
-        songs.add(Song("spotify:playlist:37i9dQZF1DX4dyzvuaRJ0n","Mint Playlist",""))
-        songs.add(Song("spotify:album:79C6OXDHPGZtCb7ySUxyIV","Holiday Songs",""))
-        songs.add(Song("spotify:playlist:37i9dQZF1DXcBWIGoYBM5M","Todays Hits",""))
-        songs.add(Song("spotify:playlist:3Di88mvYplBtkDBIzGLiiM","EDM Hits",""))
-        songs.add(Song("spotify:playlist:37i9dQZF1DWXRqgorJj26U","Rock Classics",""))
+        songs.add(Song("spotify:playlist:37i9dQZF1DX4dyzvuaRJ0n",
+            "Mint Playlist",
+            "spotify:image:ab67616d0000b273e49b1bcaa060156dd2019e17"))
+        songs.add(Song("spotify:album:79C6OXDHPGZtCb7ySUxyIV","Holiday Songs",
+            "spotify:image:ab67616d0000b27356a8fbe72905057674ac41f7"))
+        songs.add(Song("spotify:playlist:37i9dQZF1DXcBWIGoYBM5M","Todays Hits","spotify:image:ab67616d0000b27333b8541201f1ef38941024be"))
+        songs.add(Song("spotify:playlist:3Di88mvYplBtkDBIzGLiiM","EDM Hits","spotify:image:ab67616d0000b273b6567be9f8b996a2b5f9b7fa"))
+        songs.add(Song("spotify:playlist:37i9dQZF1DWXRqgorJj26U","Rock Classics","spotify:image:ab67616d0000b273fc4f17340773c6c3579fea0d"))
 
 
         // Create adapter passing in the sample user data
-        val adapter = ContactsAdapter(songs, itemOnClick)
+        val adapter = ContactsAdapter(songs, itemOnClick, setImageCover)
         // Attach the adapter to the recyclerview to populate items
         rvSongs.adapter = adapter
         // Set layout manager to position the items
@@ -53,10 +57,20 @@ class DashboardFragment : Fragment() {
     }
 
 
+
+
     val itemOnClick: (View, Int, Int) -> Unit = { view, position, type ->
         var song = songs.get(position)
         (activity as Main_Screen).playSong(song.getUri())
-        Log.d("Hello", song.getName())
+        Log.d("Hello", song.getImageURL())
+    }
+
+    val setImageCover: (ImageView, Int) -> Unit = { view, position ->
+        var song = songs.get(position)
+        (activity as Main_Screen).getTrackCoverArt(song.getImageURL())!!
+            .setResultCallback { bitmap ->
+                view.setImageBitmap(bitmap)
+            }
     }
 }
 
